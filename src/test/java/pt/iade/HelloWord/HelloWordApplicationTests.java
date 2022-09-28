621dbb3a5d26bd83c7c662d38f90d46eb39b869b
PaulinoMuya <paulinomuya63@gmail.com>
2022-09-28 15:07:44 +0100

    First commit of Hello World project

---
+++ src/test/java/pt/iade/HelloWord/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.HelloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
